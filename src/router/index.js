692f776e109b2240b865bd8b1004ccc67f6ac7c7
italloalexsander <italloalexsander@gmail.com>
2022-10-17 20:35:16 -0300

    Start set up for nav bar adaptation

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Products from '../views/Products.vue'
 
 const routes = [
   {
@@ -8,12 +9,12 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/products',
+    name: 'products',
+    component: ProductsView
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
