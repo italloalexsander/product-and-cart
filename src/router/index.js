13128010a2114403f96dc143ff2b32182fc0231a
italloalexsander <italloalexsander@gmail.com>
2022-10-17 21:08:58 -0300

    Add proper routing and PastOrdersView route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductsView from '../views/ProductsView.vue'
+import PastOrdersView from '../views/PastOrdersView.vue'
 
 const routes = [
   {
@@ -15,6 +16,11 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
+  },
+  {
+    path: '/past-orders',
+    name: 'PastOrders',
+    component: PastOrdersView
   }
 ]
 
